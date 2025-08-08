e88f256ee02c72569c70ea98e6adf85edfcfdca7
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-08-08 18:56:50 +0530

    Update test_generalized_linear_model.py

--- tests/test_generalized_linear_model.py
+++ tests/test_generalized_linear_model.py
@@ -1,81 +1,56 @@
-# tests/test_generalized_linear_model.py
-
 import numpy as np
 import pytest
 import pandas as pd
-from statisticapy.models.generalized_linear_model import GeneralizedLinearModel, LogisticRegression, PoissonRegression
-from statisticapy.models.generalized_linear_model import Gaussian, Binomial, Poisson
+from statisticapy.models.generalized_linear_model import LogisticRegression, PoissonRegression
 
-def test_glm_fit_predict_numeric():
-    X = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
-    y = np.array([1, 2, 3, 4])
-    glm = GeneralizedLinearModel(family=Gaussian(), fit_intercept=True)
-    glm.fit(X, y)
-    preds = glm.predict(X)
-    assert preds.shape == (X.shape[0],)
-
-def test_glm_fit_predict_formula():
+def test_predict_with_formula_and_dataframe_logistic():
     data = pd.DataFrame({
-        'y': [0, 1, 0, 1, 1],
-        'x1': [1, 2, 3, 4, 5],
-        'x2': [5, 4, 3, 2, 1]
+        'y': [0, 1, 0, 1],
+        'x1': [1, 2, 3, 4],
+        'x2': [4, 3, 2, 1]
     })
     formula = 'y ~ x1 + x2'
     
-    glm = GeneralizedLinearModel(family=Binomial(), formula=formula)
-    glm.fit(data=data)
-    # Prediction with numeric array should work if intercept handled
-    X_new = np.array([[6, 0], [7, -1]])
-    preds = glm.predict(X_new)
-    assert preds.shape == (2,)
-
-def test_logistic_regression_with_formula():
-    data = pd.DataFrame({
-        'y': [0, 1, 0, 1, 1],
-        'x1': [0.5, 1.2, 1.8, 3.0, 2.9],
-        'x2': [1.2, 3.1, 2.8, 0.5, 2.7]
-    })
-    formula = 'y ~ x1 + x2'
     model = LogisticRegression(formula=formula)
     model.fit(data=data)
-    preds = model.predict(np.array([[2, 1], [1, 0.5]]))
+    
+    new_data = pd.DataFrame({
+        'x1': [5, 6],
+        'x2': [0, -1]
+    })
+    
+    preds = model.predict(new_data)
     assert preds.shape == (2,)
-    assert np.all((preds >= 0) & (preds <= 1))  # Probabilities
+    assert np.all((preds >= 0) & (preds <= 1))
+    
+    # Non-DataFrame input should raise TypeError
+    with pytest.raises(TypeError):
+        model.predict(np.array([[5, 0], [6, -1]]))
 
-def test_poisson_regression_with_formula():
+def test_predict_with_formula_and_dataframe_poisson():
     data = pd.DataFrame({
-        'y': [1, 3, 7, 6, 8],
-        'x1': [1, 2, 3, 4, 5],
-        'x2': [0.5, 1.5, 3.5, 0.5, 2.0]
+        'y': [1, 2, 3, 4],
+        'x1': [1, 3, 5, 7],
+        'x2': [2, 4, 6, 8]
     })
     formula = 'y ~ x1 + x2'
+    
     model = PoissonRegression(formula=formula)
     model.fit(data=data)
-    preds = model.predict(np.array([[6, 1], [7, 2]]))
+    
+    new_data = pd.DataFrame({
+        'x1': [9, 11],
+        'x2': [10, 12]
+    })
+    
+    preds = model.predict(new_data)
     assert preds.shape == (2,)
     assert np.all(preds >= 0)
+    
+    with pytest.raises(TypeError):
+        model.predict(np.array([[9, 10], [11, 12]]))
 
-def test_fit_with_formula_requires_data():
-    glm = GeneralizedLinearModel(formula='y ~ x1 + x2')
-    with pytest.raises(ValueError):
-        glm.fit()
-
-def test_predict_before_fit_raises():
+def test_predict_without_fit_raises():
     model = LogisticRegression()
-    X = np.array([[1, 2]])
     with pytest.raises(RuntimeError):
-        model.predict(X)
-
-def test_predict_with_formula_not_implemented():
-    data = pd.DataFrame({
-        'y': [0, 1],
-        'x1': [1, 2]
-    })
-    formula = 'y ~ x1'
-    model = LogisticRegression(formula=formula)
-    model.fit(data=data)
-    with pytest.raises(NotImplementedError):
-        model.predict(data)
-
-if __name__ == "__main__":
-    pytest.main([__file__])
+        model.predict(np.array([[1, 2]]))
